ae4a29ce1aea1d90a5f078988dc37c7e89604755
Dennis Corral <dennis.corral@outlook.com>
2022-05-24 20:59:45 -0400

    new repo

--- js/romanNumerals.js
+++ js/romanNumerals.js
@@ -1,3 +1,27 @@
 exports.toRoman = function(num) {
-
+    let output = ""
+    const romanNumeralObj = {
+        M : 1000,
+        D : 500,
+        C : 100,
+        L : 50,
+        X : 10,
+        V : 5,
+        I : 1,
+    }
+   
+    let romanArr = Object.entries(romanNumeralObj)
+    let tempNum = num
+    let romanAnsw = []
+    // for(let i = 0; i < romanArr.length; i++){
+    //     if (tempNum % romanArr[i][1] !== 0){
+    //         romanAnsw.push(romanArr[i])
+    //         tempNum = tempNum - (romanArr[i][1])
+    //     } 
+    //     console.log(tempNum)
+        
+    // }
+    console.log(parseInt(romanArr[0][1]))
+    return romanAnsw
 };
+exports.toRoman()
